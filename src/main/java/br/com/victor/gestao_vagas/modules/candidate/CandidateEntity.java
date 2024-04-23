148e7ce17221a6e42eb695e68e8be15392d34458
victor duarte <victor.d.dev@gmail.com>
2024-04-23 11:41:44 -0300

    feat: add candidate entity

---
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,14 @@
+package br.com.victor.gestao_vagas.modules.candidate;
+
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+    private String username;
+    private String password;
+    private String description;
+    private String curriculum;
+}
