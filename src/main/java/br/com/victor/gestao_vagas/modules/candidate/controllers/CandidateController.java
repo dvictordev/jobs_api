935e77568149a46c394b8961ea360eff8c08e826
victor duarte <victor.d.dev@gmail.com>
2024-04-23 11:15:34 -0300

    Feat: add CandidateController

---
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,13 @@
+package br.com.victor.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(){}
+}
