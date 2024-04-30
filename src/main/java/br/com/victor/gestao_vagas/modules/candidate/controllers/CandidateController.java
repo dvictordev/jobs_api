c24269e5a21f1d7af3e0f94fe93bd4561aed798c
Victor Duarte <victor.d.dev@gmail.com>
2024-04-30 11:49:35 -0300

    feat: :sparkles: add candidate profile function
    Add controller and use case to find the candidate profile using his id through the request

--- src/main/java/br/com/victor/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,10 +2,15 @@ package br.com.victor.gestao_vagas.modules.candidate.controllers;
 
 import br.com.victor.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import br.com.victor.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.victor.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +23,25 @@ public class CandidateController {
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
+    @Autowired
+    private ProfileCandidateUseCase profileCandidateUseCase;
+
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidate) {
         try {
             var result = this.createCandidateUseCase.execute(candidate);
             return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<Object> findCandidate(HttpServletRequest httpServletRequest) {
+        var id = httpServletRequest.getAttribute("candidate_id");
+        try {
+            var profile = this.profileCandidateUseCase.execute(UUID.fromString(id.toString()));
+            return ResponseEntity.ok().body(profile);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
---
+++ src/main/java/br/com/victor/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,32 @@
+package br.com.victor.gestao_vagas.modules.candidate.useCases;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.victor.gestao_vagas.exceptions.UserFoundException;
+import br.com.victor.gestao_vagas.modules.candidate.dto.ProfileCandidateReponseDTO;
+import br.com.victor.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import br.com.victor.gestao_vagas.modules.candidate.repositorie.CandidateRepository;
+
+@Service
+public class ProfileCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateReponseDTO execute(UUID idCandidate) throws UserFoundException {
+        var candidate = this.candidateRepository.findById(idCandidate).orElseThrow(() -> {
+            throw new UserFoundException();
+        });
+
+        var candidateReponseDTO = ProfileCandidateReponseDTO.builder().description(candidate.getDescription())
+                .email(candidate.getEmail()).name(candidate.getName()).id(candidate.getId()).build();
+
+        return candidateReponseDTO;
+
+    }
+
+}
